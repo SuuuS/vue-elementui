422a8250fee12eb067adbf9fd16cd67670523cd8
SuuuS <susang945@163.com>
2017-07-04 10:20:10 +0800

    ls 2017-7-4

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
+import router from './router/router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
-import router from './router/router'
+
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
--- src/page/fipage/riskAssessment.vue
+++ src/page/fipage/riskAssessment.vue
@@ -2,52 +2,24 @@
 <template lang="html">
   <div class="riskAssessment">
     风险评估
-    <ul>
-      <li>
-        <el-card :body-style="{ padding: '0px' }">
-          <img src="" class="image">
-          <div style="padding: 14px;">
-            <span>{{ }}</span>
-            <div class="bottom clearfix">
-              <time class="time">{{ }}</time>
-              <el-button type="text" class="button">操作按钮</el-button>
-            </div>
-          </div>
-        </el-card>
-      </li>
-    </ul>
+    <div id="mount-point"></div>
+
   </div>
 </template>
 
 <script>
-export default {
-  data() {
+var Profile = Vue.extend({
+  template: '<p>{{firstName}} {{lastName}} aka {{alias}}</p>',
+  data: function () {
     return {
-
+      firstName: 'Walter',
+      lastName: 'White',
+      alias: 'Heisenberg'
     }
-  },
-  methods: {
-    getData(){
-      // this.$http.get("http://localhost:8080/src/data.json").then((response) => {
-      //   console.log(response)
-      //   //this.
-      //
-      // }).catch(function(err){
-      //   console.log(err);
-      // })
-      this.axios.get("http://localhost:8080/src/data.json").then((res) => { // 方法二
-        // this.list = res.data.seller
-        // this.text = res.data.ratings
-        console.log(res.data)
-      }).catch(function(err){
-        console.log(err);
-      });
-    }
-  },
-  mounted(){
-      this.getData();
   }
-}
+})
+// 创建 Profile 实例，并挂载到一个元素上。
+new Profile().$mount('#mount-point');
 </script>
 
 <style lang="css">
